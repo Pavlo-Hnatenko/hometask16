15fdb165539b401e0168970ec221d552a8716ca6
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-08-18 12:00:38 +0300

    third commit: some improvement in the class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        personalities = new ArrayList<>();
+        csvTable = getCsvTable();
+        personalities = getPersonalitiesFromCsv();
+
+        printPersonalitiesProperties(personalities);
+
+    }
+
+    private static CsvTable getCsvTable(){
 
         try {
             csvTable = CsvTable.fromFile(Paths.get("origin.csv")).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return csvTable;
+    }
+
+    private static List<Personality> getPersonalitiesFromCsv() {
+
+        personalities = new ArrayList<>();
 
         for (int i = 0; i < csvTable.height(); i++) {
 
@@ -32,12 +46,7 @@ public class Main {
             ));
         }
 
-        for (Personality personality : personalities) {
-            System.out.println("personality.getName() = " + personality.getName());
-            System.out.println("personality.getAge() = " + personality.getAge());
-            System.out.println("personality.getSex() = " + personality.getSex());
-            System.out.println("personality.getOccupation() = " + personality.getOccupation());
-        }
+        return personalities;
     }
 
     private static String getPersonalityProperty(String personalityField) {
@@ -66,5 +75,26 @@ public class Main {
         }
 
         return null;
+    }
+
+    private static void printPersonalitiesProperties(List<Personality> personalities){
+
+        for (Personality personality : personalities) {
+            System.out.println("personality.getName() = " + personality.getName());
+            System.out.println("personality.getAge() = " + personality.getAge());
+            System.out.println("personality.getSex() = " + personality.getSex());
+            System.out.println("personality.getOccupation() = " + personality.getOccupation());
+        }
+    }
+
+    //    a potentially simple way to create personalities from CSV file (without reflection)
+    private static void getPersonalitiesWithoutReflection() {
+
+        for (int i = 0; i < csvTable.height(); i++) {
+
+            row = csvTable.getRow(i);
+
+            personalities.add(new Personality(row.get(0), Integer.parseInt(row.get(1)), row.get(2), row.get(3)));
+        }
     }
 }
